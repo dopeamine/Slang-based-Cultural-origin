e3ad617be52582130b02c682d11cc4f81ff8d703
dopeamine <shantanu.rajenimbalkar@gmail.com>
2017-05-02 14:59:46 -0400

    data extracted

--- test1.py
+++ test1.py
@@ -12,7 +12,20 @@ from nltk.corpus import stopwords #stopwords
 from nltk.corpus import words # english vocabulary
 
 special_char = ["!","$","%","^","&","*","(",")","[","]","{","}","|","\",""","\\",":",";","?","/",".",">",",","<","+","-","=","_"]
-    
+   
+regions = {'northeast' : ['ME','VT','NH','NY','MA','CT','RI','PA','NJ'],
+             'midwest': ['ND','MN','SD','NE','IA','KS','MO','IL','WI','MI','OH','IN'],
+             'west': ['WA','MT','OR','ID','WY','CA','NV','UT','CO','AZ','NM'],
+             'south': ['OK','TX','AR','LA','KY','TN','MS','AL','WV','VA','MD','DE','NC','SC','GA','FL'],
+             'pacific': ['AK','HI']
+             } 
+
+def findRegion(state):
+    for region in regions:
+        if state in regions[region]:
+            return region
+    return False
+
 def preprocess(text):
 #    text = [char for char in text if char not in special_char]
     text = re.sub(r'RT ','',text) #remove 'RT '
@@ -32,16 +45,32 @@ def preprocess(text):
     
     
 all_text=[]
+us_coord_only = {}
+with open('latlong_us_only_state_abrv.csv') as csvfile:
+    reader = csv.reader(csvfile,delimiter=',')
+    for row in reader:
+        location = row[0]+","+row[1]
+        us_coord_only[location] = row[2]
+        
 i=0
 with open("je_2010.txt",encoding = "ISO-8859-1") as tsv:
     tsv = (x.replace('\0', '') for x in tsv)
-    for test in csv.reader(tsv, delimiter="\t"): #You can also use delimiter="\t" rather than giving a dialect.
-#        print('----------------------------------')
-#        print(test[5])
-        text = preprocess(test[5]) 
-        print(text)
-        i=i+1
-        if i==1000:
-#        all_text.append(test[5])
-            break;
+    with open('main_data.csv','w',newline='') as csvfile:
+        main_data_writer = csv.writer(csvfile,delimiter=',')
+        for test in csv.reader(tsv, delimiter="\t"): #You can also use delimiter="\t" rather than giving a dialect.
+    #        print('----------------------------------')
+#            print(test)
+            user_id = test[0]
+            tweet_text = preprocess(test[5])
+            location = test[3]+","+test[4]
+            if location in us_coord_only:
+                state = us_coord_only[location]
+                region = findRegion(state)
+                main_data = [user_id, tweet_text, test[3],test[4],state, region]
+#                print(tweet_text)
+                main_data_writer.writerow(main_data)
+#                i=i+1
+#                if i==10:
+    #        all_text.append(test[5])
+#                    break;
 
